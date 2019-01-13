9f9633f55025061466379e5aef0d72a2ad4ac248
Shing Lyu <shing.lyu@gmail.com>
2019-01-13 22:39:48 +0100

    Added list.insert()

--- tests/snippets/list.py
+++ tests/snippets/list.py
@@ -12,6 +12,7 @@ assert y == [2, 1, 2, 3, 1, 2, 3]
 assert x * 0 == [], "list __mul__ by 0 failed"
 assert x * 2 == [1, 2, 3, 1, 2, 3], "list __mul__ by 2 failed"
 
+# index()
 assert ['a', 'b', 'c'].index('b') == 1
 assert [5, 6, 7].index(7) == 2
 try:
@@ -20,3 +21,20 @@ except ValueError:
     pass
 else:
     assert False, "ValueError was not raised"
+
+# insert()
+x = ['a', 'b', 'c']
+x.insert(0, 'z') # insert is in-place, no return value
+assert x == ['z', 'a', 'b', 'c']
+
+x = ['a', 'b', 'c']
+x.insert(100, 'z')
+assert x == ['a', 'b', 'c', 'z']
+
+x = ['a', 'b', 'c']
+x.insert(-1, 'z')
+assert x == ['a', 'b', 'z', 'c']
+
+x = ['a', 'b', 'c']
+x.insert(-100, 'z')
+assert x == ['z', 'a', 'b', 'c']
