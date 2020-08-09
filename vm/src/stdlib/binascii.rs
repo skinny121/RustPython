e58a954189a77d4dbd90f6d79aad153980d3567d
Ben Lewis <benlewisj@gmail.com>
2020-08-09 12:16:19 +1200

    Add AsciiBytesLike to accept both an ascii string or a buffer.

--- vm/src/stdlib/binascii.rs
+++ vm/src/stdlib/binascii.rs
@@ -2,54 +2,12 @@ pub(crate) use decl::make_module;
 
 #[pymodule(name = "binascii")]
 mod decl {
-    use crate::byteslike::PyBytesLike;
+    use crate::byteslike::{PyAsciiBytesLike, PyBytesLike};
     use crate::function::OptionalArg;
-    use crate::obj::objbytearray::{PyByteArray, PyByteArrayRef};
-    use crate::obj::objbytes::{PyBytes, PyBytesRef};
-    use crate::obj::objstr::{PyString, PyStringRef};
-    use crate::pyobject::{PyObjectRef, PyResult, TryFromObject, TypeProtocol};
+    use crate::pyobject::PyResult;
     use crate::vm::VirtualMachine;
     use crc::{crc32, Hasher32};
     use itertools::Itertools;
-
-    enum SerializedData {
-        Bytes(PyBytesRef),
-        Buffer(PyByteArrayRef),
-        Ascii(PyStringRef),
-    }
-
-    impl TryFromObject for SerializedData {
-        fn try_from_object(vm: &VirtualMachine, obj: PyObjectRef) -> PyResult<Self> {
-            match_class!(match obj {
-                b @ PyBytes => Ok(SerializedData::Bytes(b)),
-                b @ PyByteArray => Ok(SerializedData::Buffer(b)),
-                a @ PyString => {
-                    if a.as_str().is_ascii() {
-                        Ok(SerializedData::Ascii(a))
-                    } else {
-                        Err(vm.new_value_error(
-                            "string argument should contain only ASCII characters".to_owned(),
-                        ))
-                    }
-                }
-                obj => Err(vm.new_type_error(format!(
-                    "argument should be bytes, buffer or ASCII string, not '{}'",
-                    obj.class().name,
-                ))),
-            })
-        }
-    }
-
-    impl SerializedData {
-        #[inline]
-        pub fn with_ref<R>(&self, f: impl FnOnce(&[u8]) -> R) -> R {
-            match self {
-                SerializedData::Bytes(b) => f(b.get_value()),
-                SerializedData::Buffer(b) => f(&b.borrow_value().elements),
-                SerializedData::Ascii(a) => f(a.as_str().as_bytes()),
-            }
-        }
-    }
 
     fn hex_nibble(n: u8) -> u8 {
         match n {
@@ -81,29 +39,35 @@ mod decl {
         }
     }
 
-    #[pyfunction(name = "a2b_hex")]
-    #[pyfunction]
-    fn unhexlify(data: SerializedData, vm: &VirtualMachine) -> PyResult<Vec<u8>> {
-        data.with_ref(|hex_bytes| {
-            if hex_bytes.len() % 2 != 0 {
-                return Err(vm.new_value_error("Odd-length string".to_owned()));
-            }
+    fn unhexlify_internal(hex_bytes: &[u8], vm: &VirtualMachine) -> PyResult<Vec<u8>> {
+        if hex_bytes.len() % 2 != 0 {
+            return Err(vm.new_value_error("Odd-length string".to_owned()));
+        }
 
-            let mut unhex = Vec::<u8>::with_capacity(hex_bytes.len() / 2);
-            for (n1, n2) in hex_bytes.iter().tuples() {
-                if let (Some(n1), Some(n2)) = (unhex_nibble(*n1), unhex_nibble(*n2)) {
-                    unhex.push(n1 << 4 | n2);
-                } else {
-                    return Err(vm.new_value_error("Non-hexadecimal digit found".to_owned()));
-                }
+        let mut unhex = Vec::<u8>::with_capacity(hex_bytes.len() / 2);
+        for (n1, n2) in hex_bytes.iter().tuples() {
+            if let (Some(n1), Some(n2)) = (unhex_nibble(*n1), unhex_nibble(*n2)) {
+                unhex.push(n1 << 4 | n2);
+            } else {
+                return Err(vm.new_value_error("Non-hexadecimal digit found".to_owned()));
             }
+        }
 
-            Ok(unhex)
-        })
+        Ok(unhex)
     }
 
     #[pyfunction]
-    fn crc32(data: SerializedData, value: OptionalArg<u32>, vm: &VirtualMachine) -> PyResult {
+    fn a2b_hex(data: PyAsciiBytesLike, vm: &VirtualMachine) -> PyResult<Vec<u8>> {
+        data.with_ref(|hex_bytes| unhexlify_internal(hex_bytes, vm))
+    }
+
+    #[pyfunction]
+    fn unhexlify(data: PyBytesLike, vm: &VirtualMachine) -> PyResult<Vec<u8>> {
+        data.with_ref(|hex_bytes| unhexlify_internal(hex_bytes, vm))
+    }
+
+    #[pyfunction]
+    fn crc32(data: PyBytesLike, value: OptionalArg<u32>, vm: &VirtualMachine) -> PyResult {
         let crc = value.unwrap_or(0);
 
         let mut digest = crc32::Digest::new_with_initial(crc32::IEEE, crc);
@@ -128,7 +92,7 @@ mod decl {
     }
 
     #[pyfunction]
-    fn a2b_base64(s: SerializedData, vm: &VirtualMachine) -> PyResult<Vec<u8>> {
+    fn a2b_base64(s: PyAsciiBytesLike, vm: &VirtualMachine) -> PyResult<Vec<u8>> {
         s.with_ref(|b| base64::decode(trim_newline(b)))
             .map_err(|err| vm.new_value_error(format!("error decoding base64: {}", err)))
     }
